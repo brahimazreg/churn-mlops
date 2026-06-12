b9bf22976052db6ed0bf1d3a6284a2007d91bda5
Brahim <brahimaz@hotmail.com>
2026-06-12 14:18:56 +0200

    Add GitHub Actions CI

--- src/api.py
+++ src/api.py
@@ -5,11 +5,18 @@ from src.predict import predict
 from src.config import MODEL_PATH
 from pydantic import BaseModel
 
-app = FastAPI(title="Churn Prediction API")
+app = FastAPI(title="Churn Prediction API",
+              description="Predict customer churn using Logistic Regression",
+              version="1.0.0"
+              )
 
 model = load(MODEL_PATH)
 
 from pydantic import BaseModel
+
+class PredictionResponse(BaseModel):
+    prediction: int
+    probability: float
 
 class CustomerInput(BaseModel):
     gender: str
@@ -38,14 +45,15 @@ def home():
     return {"message": "Churn Prediction API is running"}
 
 
-@app.post("/predict")
+@app.post("/predict" , response_model=PredictionResponse)
 def predict_api(data: CustomerInput):
     df = pd.DataFrame([data.model_dump()])
-    #df = pd.DataFrame([data])
-
     pred, prob = predict(df)
 
     return {
         "prediction": int(pred[0]),
         "probability": float(prob[0])
     }
+@app.get("/health")
+def health():
+    return {"status": "healthy"}
